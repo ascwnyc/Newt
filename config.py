de06f78a4c1dc4e0d6a9e230f9ab4bb10056a38d
Alex Williams <88713688+ascwnyc@users.noreply.github.com>
2022-02-10 10:46:34 +0000

    Deprecated old reddit functionality.

--- config.py
+++ config.py
@@ -10,20 +10,6 @@ DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
 REDDIT_APP_ID = os.environ['REDDIT_APP_ID']
 REDDIT_APP_TOKEN = os.environ['REDDIT_APP_TOKEN']
 
-
-# Old subreddit query system, currently inactive
-REDDIT_SFW_SUBREDDITS = [
-    '196', 
-    ]
-REDDIT_NSFW_SUBREDDITS = [
-    'nsfw',   
-    ]
-REDDIT_CATEGORIES = [
-    'hot',
-    'new',
-    'top',
-]
-
 # // Bot Strings
 # bot_avatar = "https://i.imgur.com/ayk78Kg.png" # deprecated, uses 'self.bot.user.avatar_url' instead.
 bot_url = "https://ascwnyc.github.io/Newt/"
